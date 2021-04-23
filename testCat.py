8a5edeba292c26102ac910b2e09ff698ed5c73ac
AidysM <mongush10011979@gmail.com>
2021-04-24 02:32:21 +0700

    first commit

---
+++ testCat.py
@@ -0,0 +1,23 @@
+from cat import Cat
+
+c1 = Cat('Барон', 'мальчик', 2)
+c2 = Cat('Сэм', 'мальчик', 2)
+c3 = Cat('Придуманная', 'девочка', 3)
+
+print("c1.name=", c1.name)
+print("c1.sex=", c1.sex)
+print("c1.age=", c1.age)
+print("c1.getName=", c1.getName())
+print("c1.getSex=", c1.getSex())
+print("c1.getAge=", c1.getAge())
+
+print("c2.name=", c2.name)
+print("c2.sex=", c2.sex)
+print("c2.age=", c2.age)
+print("c2.getName=", c2.getName())
+print("c2.getSex=", c2.getSex())
+print("c2.getAge=", c2.getAge())
+
+print("c3.getName=", c3.getName())
+print("c3.getSex=", c3.getSex())
+print("c3.getAge=", c3.getAge())
